e9e626cf7993e1d5c585c2c727660421ea5e0a56
James Phillips <jmphil09@gmail.com>
2019-06-16 13:43:32 -0700

    Cleaned up random_agent.py.

--- random_agent.py
+++ random_agent.py
@@ -1,26 +1,13 @@
 import retro
 
 
-def main():
-    env = retro.make(game='SuperMarioBros-Nes', state='Level2-1.state')
-    obs = env.reset()
-    done = False
-    while not done:
-        #action will be given by the neural net
-        action = env.action_space.sample()
-        action = [0, 0, 0, 0, 0, 0, 0, 1, 0]
-        obs, reward, done, info = env.step(action)
-        env.render()
-        print('obs.shape: {}, reward: {}, done: {}, info: {}, action: {}'.format(obs.shape, reward, done, info, action))
-    env.close()
+env = retro.make(game='SuperMarioBros-Nes', state='Level1-1.state')
+env.reset()
 
-
-if __name__ == "__main__":
-    main()
-
-'''
-import retro
-print(retro.data.list_games())
-
-python -m retro.import .
-'''
+done = False
+while not done:
+    env.render()
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    print('obs.shape: {}, reward: {}, done: {}, info: {}, action: {}'.format(obs.shape, reward, done, info, action))
+env.close()
