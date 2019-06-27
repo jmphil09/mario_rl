b1f17ab3891abe7f517c81a998ecca8c7a4f4a93
James Phillips <jmphil09@gmail.com>
2019-06-26 23:27:39 -0700

    Getting ready for testing hyperparameters in parallel.

--- hyperparam_test.py
+++ hyperparam_test.py
@@ -3,8 +3,8 @@ from GameRunner import GameRunner
 
 if __name__ == '__main__':
     runner = GameRunner(
-        num_threads=1,
-        show_game=True,
+        num_threads=16,
+        show_game=False,
         show_nn_view=False,
         level_end_score=3186,
         convolution_weight=8,
