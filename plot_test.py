fa7150a031b3ec5dbb4ddd5f7c43502528eca424
James Phillips <jmphil09@gmail.com>
2019-07-09 21:50:59 -0700

    More hyperparam testing.

--- plot_test.py
+++ plot_test.py
@@ -1,7 +1,7 @@
 from FitnessPlot import FitnessPlot
 
 
-plot = FitnessPlot(folder_prefix='data_pop50')
+plot = FitnessPlot(folder_prefix='data')
 worker_dict = plot.create_worker_dict()
 
 plot.plot_all_workers()
