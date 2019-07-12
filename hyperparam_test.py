6d5caf79a57581a763482a58f6780f0304919c7b
James Phillips <jmphil09@gmail.com>
2019-07-11 17:05:56 -0700

    Generated more data.

--- hyperparam_test.py
+++ hyperparam_test.py
@@ -3,13 +3,13 @@ from GameRunner import GameRunner
 
 if __name__ == '__main__':
     runner = GameRunner(
-        num_threads=16,
+        num_threads=32,
         show_game=False,
         show_nn_view=False,
         level_end_score=3186,
         convolution_weight=8,
         config_file_name='config',
-        worker_start_num=16
+        worker_start_num=0
     )
 
     runner.run_all_threads()
--- plot_test.py
+++ plot_test.py
@@ -1,7 +1,7 @@
 from FitnessPlot import FitnessPlot
 
 
-plot = FitnessPlot(folder_prefix='data')
+plot = FitnessPlot(folder_prefix='data', num_workers=32)
 worker_dict = plot.create_worker_dict()
 
 plot.plot_all_workers()
