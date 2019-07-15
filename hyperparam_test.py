42968ed53e79a998ef63df2b09a2042c25b10f21
James Phillips <jmphil09@gmail.com>
2019-07-15 16:08:10 -0700

    Gathered data to compare convolution levels and performance.

--- hyperparam_test.py
+++ hyperparam_test.py
@@ -3,14 +3,14 @@ from GameRunner import GameRunner
 
 if __name__ == '__main__':
     runner = GameRunner(
-        num_threads=16,
+        num_threads=1,
         show_game=False,
         show_nn_view=False,
         level_end_score=3186,
-        convolution_weight=8,
+        convolution_weight=6,
         config_file_name='config',
-        worker_start_num=16,
-        max_generation=300
+        worker_start_num=3,
+        max_generation=100
     )
 
     runner.run_all_threads()
--- hyperparam_test2.py
+++ hyperparam_test2.py
@@ -3,14 +3,14 @@ from GameRunner import GameRunner
 
 if __name__ == '__main__':
     runner = GameRunner(
-        num_threads=16,
+        num_threads=1,
         show_game=False,
         show_nn_view=False,
         level_end_score=3186,
         convolution_weight=8,
         config_file_name='config',
-        worker_start_num=0,
-        max_generation=300
+        worker_start_num=28,
+        max_generation=100
     )
 
     runner.run_all_threads()
