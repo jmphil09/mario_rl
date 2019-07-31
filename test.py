896d2a3f5d8f36a3a082c15357992281da472324
James Phillips <jmphil09@gmail.com>
2019-07-30 18:51:03 -0700

    Working more on hyperparam testing.

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     except Exception as ex:
         print("data directory does not exist")
         print(ex)
-    N = 12
+    N = 6
     worker_start_num = 0
     worker_end_num = N
     config = ConfigGenerator()
@@ -19,7 +19,9 @@ if __name__ == '__main__':
     tuner = HyperparamTuner(config_dict)
     result = tuner.run_multiple_workers(worker_start_num, worker_end_num)
     score_list = []
+
     for n in range(0, N):
         score_list.append(tuner.print_output(n)[1])
+    print(config_dict)
     print("Max Score: {}".format(max(score_list)))
     print("Average Score: {}".format(sum(score_list)/len(score_list)))
