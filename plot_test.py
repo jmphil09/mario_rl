deafc34e3c4a54c0a887f0c9b2eea22b8c83ed6c
James Phillips <jmphil09@gmail.com>
2019-08-07 20:02:19 -0700

    Finished writing a script to create random data for hyperparameter searching.

--- plot_test.py
+++ plot_test.py
@@ -1,7 +1,7 @@
 from FitnessPlot import FitnessPlot
 
 
-plot = FitnessPlot(folder_prefix='data', num_workers=32)
+plot = FitnessPlot(folder_prefix='hyperparam_data/1565232031', num_workers=8)
 worker_dict = plot.create_worker_dict()
 
 plot.plot_all_workers()
--- random_hyperparam_search.py
+++ random_hyperparam_search.py
@@ -1,11 +1,17 @@
+import os
+import time
+
+from pathlib import Path
+from shutil import copyfile, rmtree
+
 from ConfigGenerator import ConfigGenerator
 from GameRunner import GameRunner
 
-N = 8
+N = 16
 M = 10
 
 
-if __name__ == '__main__':
+def main():
     #Generate a random config file
     config = ConfigGenerator()
     config.randomize()
@@ -17,9 +23,30 @@ if __name__ == '__main__':
     runner = GameRunner(num_threads=N, show_game=True, max_generation=M)
     runner.run_all_threads()
 
-    #After all workers are finished, move the fitness list files to
-    #done/data_timestamp
+    #After all workers are finished, move the fitness list files to a data directory
+    timestamp = str(time.time()).split('.')[0]
+    config_path = Path('config')
+    config_destination_path = Path('hyperparam_data/{}/config'.format(timestamp))
+    os.makedirs(os.path.dirname(config_destination_path), exist_ok=True)
+    copyfile(config_path, config_destination_path)
+    for n in range(N):
+        src_path = Path('data/config_{}/worker-{}-fitness_list.pkl'.format(n, n))
+        destination_path = Path('hyperparam_data/{}/worker-{}-fitness_list.pkl'.format(timestamp, n))
+        copyfile(src_path, destination_path)
 
     #clear the data folder
+    def clear_data_dir(dir='data'):
+        try:
+            rmtree(dir)
+        except Exception as ex:
+            print("{} directory does not exist".format(dir))
+            print(ex)
 
-    #repeat
+    clear_data_dir()
+
+    del runner
+
+
+if __name__ == '__main__':
+    for n in range(10):
+        main()
