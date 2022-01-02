90f4a7c0d4067d89981c856c8e1c2f757cb80e6d
James Phillips <jmphil09@gmail.com>
2022-01-02 13:53:28 -0800

    Working on a faster way to load fitness plots.

--- FitnessPlot_v6.py
+++ FitnessPlot_v6.py
@@ -27,6 +27,7 @@ class FitnessPlot:
             return (generation, fitness_list)
         except Exception as ex:
             print(ex)
+            print('Error occurred on: {}'.format(checkpoint_filename))
             return (None, None)
 
     def _create_fitness_list_for_checkpoint_par(self, core_num, file_list):
@@ -57,47 +58,54 @@ class FitnessPlot:
             result[key] = value
         return result
 
-    def _plot_max_values(self):
-        plt.figure(1)
-        folders = glob.glob(str(Path('{}/*'.format(self.folder_prefix))))
-        for subfolder in folders:
-            plot_list = []
-            fitness_dict = self.create_fitness_dict_par(subfolder)
-            plot_list = sorted([(key, max([0 if x is None else x for x in value])) for key, value in fitness_dict.items()])
-            plot_list = [x2 for (x1, x2) in plot_list]
-            label = subfolder.split('/')[-1]
-            plt.plot(plot_list, label='{}'.format(label))
-        plt.ylabel('Max Fitness')
-        plt.xlabel('Generation')
-        plt.legend()
+    def create_fitness_dict_with_cache(self, subfolder):
+        # check for existing pickled fitness list
+        existing_fitness_dict = {}# {'test_key': 'test_value', 7041: [1,2,3], 7042: [1,2,3], 7043: [1,2,3], 7049: [1,2,3]}
+        existing_fitness_dict = self._load_fitness_dict_from_pickle(subfolder)
+        #print(existing_fitness_dict)
+        # check for new checkpoint files (see if there are 2 or more?)
+        # update existing pickled fitness list by adding new checkpoint data
+        new_fitness_dict = self.create_fitness_dict_par(subfolder)
+        new_fitness_dict.update(existing_fitness_dict)
+        self._save_fitness_dict_as_pickle(new_fitness_dict, subfolder)
+        # delete added checkpoint files
+        #TODO: delete all checkpoints except the most recent one
+        # return updated pickled fitness list
+        return new_fitness_dict
 
-    def _plot_average_values(self):
-        plt.figure(2)
-        folders = glob.glob(str(Path('{}/*'.format(self.folder_prefix))))
-        for subfolder in folders:
-            plot_list = []
-            fitness_dict = self.create_fitness_dict_par(subfolder)
-            plot_list = sorted([(key, sum([0 if x is None else x for x in value]) / len(value)) for key, value in fitness_dict.items()])
-            plot_list = [x2 for (x1, x2) in plot_list]
-            label = subfolder.split('/')[-1]
-            plt.plot(plot_list, label='{}'.format(label))
-        plt.ylabel('Average Fitness')
-        plt.xlabel('Generation')
-        plt.legend()
+
+    def _save_fitness_dict_as_pickle(self, fitness_dict, subfolder):
+        pickle_name = Path('{}/fitness_dict.pkl'.format(subfolder))
+        #pickle_dir = pickle_name.parent
+        #pickle_dir.mkdir(parents=True, exist_ok=True)
+        #with open(pickle_name, 'wb') as output:
+        #    pickle.dump(fitness_dict, output, 1)
+        with gzip.open(pickle_name, 'w', compresslevel=5) as f:
+            data = fitness_dict
+            pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def _load_fitness_dict_from_pickle(self, subfolder):
+        result = {}
+        try:
+            pickle_name = Path('{}/fitness_dict.pkl'.format(subfolder))
+            with gzip.open(pickle_name) as output:
+                result = pickle.load(output)
+        except Exception as ex:
+            print(ex)
+        return result
 
     def _plot_max_and_avg(self):
         '''
-        This function plots both the max and average while only calculating the fitness_dict 1 time. This runs in half the time of manually running both.
-        Output from a test script:
-        Runtime max: 90s
-        Runtime avg: 87s
-        Runtime both: 91s
+        This function plots both the max and average fitness scores.
         '''
         folders = glob.glob(str(Path('{}/*'.format(self.folder_prefix))))
         max_gen = 1
         for subfolder in folders:
             plot_list = []
-            fitness_dict = self.create_fitness_dict_par(subfolder)
+            fitness_dict = self.create_fitness_dict_with_cache(subfolder)
+            #print(sorted(fitness_dict.keys()))
+            #print(len(fitness_dict.keys()))
+            #print(fitness_dict[7045])
             max_gen = max(max_gen, len(fitness_dict))
             plt.figure(1)
             plot_list = sorted([(key, max([0 if x is None else x for x in value])) for key, value in fitness_dict.items()])
